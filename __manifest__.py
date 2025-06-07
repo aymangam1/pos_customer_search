7af6aae77fbbaea3889a801476c72732a4afc77e
aymangam1 <aymanfm3001@gmail.com>
2025-06-07 20:10:14 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pos Customer Search",
+    'summary': "search & create customer",
+    'author': "Ayman",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['point_of_sale'],
+    'data': [
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_customer_search/static/src/css/partner_search_bar.css',
+            'pos_customer_search/static/src/js/custo_partne_button1.js',
+            'pos_customer_search/static/src/xml/custo_partne_button_view1.xml',
+        ],
+    },
+}
+
